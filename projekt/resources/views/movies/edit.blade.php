e971146d6dbd8cc50934d1677c053c9b7ae5efe8
Amanda Jakobsson <Amanda@Amandas-MacBook-Air.local>
2020-06-15 13:48:39 +0200

    indeterat

--- projekt/resources/views/movies/edit.blade.php
+++ projekt/resources/views/movies/edit.blade.php
@@ -3,28 +3,18 @@
 @section('content')
 
 <h1>Edit review</h1>
-
-
 <div class="container" style="min-height: 800px;">
 <form method="POST" action="/reviews/{{$review->id}}">
-
     {{method_field('PATCH')}}
     {{ csrf_field()}}
-
-    
-    
     <input type="hidden" name="" value="">
-
-   
     <div class="form-group green-border-focus">
         <label for="exampleFormControlTextarea5"></label>
         <textarea name="comments" class="form-control {{$errors->has('comments') ? 'is-danger' : ''}}" >{{ old('comments') }}</textarea>
     </div>
     <div>
-       
-    <button type="submit" class="btn btn-secondary">Create</button>
+        <button type="submit" class="btn btn-secondary">Create</button>
     </div>
-
     @if ($errors->any())
         <div class="">
             <ul>
@@ -34,7 +24,6 @@
             </ul>
         </div>
     @endif
-
 </form>
 </div>
 
